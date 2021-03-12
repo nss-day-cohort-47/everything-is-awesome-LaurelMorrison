83b2bf0520fcae5e79c1c786eae82f5e359e1743
Laurel Morrison <lbentleym1992@gmail.com>
2021-03-12 11:42:31 -0500

    adding progress

--- scripts/legos/LegoDetail.js
+++ scripts/legos/LegoDetail.js
@@ -3,8 +3,9 @@ import { invertColor } from './../helpers.js';
 export const LegoDetail = (brick) => {
 
   let block = `<section class="block-wrapper" style="background-color:#${brick.ColorHex}">
-              <h3>Name: ${brick.LegoName}</h3>
+              <h3>Name: ${brick.LegoName.toUpperCase()}</h3>
               <div class="block-years">Manufactured ${brick.YearFrom} - ${brick.YearTo}</div>
+              <div class="Notes">${brick.Notes}</div>             
               `;
   const link = brick.ColorstreamLinkImage;
   if (link) {
---
+++ scripts/navBar.js
@@ -0,0 +1,21 @@
+export const dropDownNav = () => {
+    return
+    `<div id="dropdown">
+        <button class="dropbtn">Select Material
+        <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+            <a href="${brick.Material}">Solid</a>
+            <a href="${brick.Material}">Transparent</a>
+            <a href="${brick.Material}">Pearl</a>
+            <a href="${brick.Material}">Chrome</a>
+            <a href="${brick.Material}">Metallic</a>
+            <a href="${brick.Material}">Milky</a>
+            <a href="${brick.Material}">Glitter</a>
+            <a href="${brick.Material}">Speckle</a>
+            <a href="${brick.Material}">Ink</a>
+            <a href="${brick.Material}">Process</a>
+            <a href="${brick.Material}">Modulex</a>
+        </div>
+  `
+}
